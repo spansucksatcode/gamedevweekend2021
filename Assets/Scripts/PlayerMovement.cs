f095e9ab0eee59d1db849e59be5ee02ea35b5f75
Spandan More <spandanmore100@gmail.com>
2021-04-18 13:14:59 +0530

    Initial Commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float jumpForce = 6.0f;
+    float movementSpeed = 5.0f;
+
+    Rigidbody playerBody;
+    float horizontalMovement = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMovement = Input.GetAxis("Horizontal");
+
+        playerBody.velocity = new Vector3(horizontalMovement * movementSpeed, playerBody.velocity.y, 0f);
+
+        if(Input.GetKeyDown(KeyCode.Space)) 
+        {
+            playerBody.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+            Debug.Log("The Spacebar has been pressed.");
+        }
+    }
+}
